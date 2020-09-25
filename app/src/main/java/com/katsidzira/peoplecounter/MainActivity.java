b1d0381e72e993149f8a771864b986a9080e264c
Chelsea Katsidzira <ckatsidzira322@gmail.com>
2020-09-25 00:24:21 -0400

    set up view

--- app/src/main/java/com/katsidzira/peoplecounter/MainActivity.java
+++ app/src/main/java/com/katsidzira/peoplecounter/MainActivity.java
@@ -2,12 +2,23 @@ package com.katsidzira.peoplecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView peopleTv, totalTv;
+    Button addButton, resetButton, removeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        peopleTv = findViewById(R.id.textview_people);
+        totalTv = findViewById(R.id.textview_total);
+        addButton = findViewById(R.id.button_add);
+        resetButton = findViewById(R.id.button_reset);
+        removeButton = findViewById(R.id.button_remove);
+
     }
 }
